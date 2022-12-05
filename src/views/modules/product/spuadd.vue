0517f0198dca3c14bbdd1fd1537309d07522af1a
xiaobailer <th15986581153@gmail.com>
2022-12-05 11:11:34 +0800

    对spuadd.vue修改，跳过图片商品名字等校验，将required改为了false

--- src/views/modules/product/spuadd.vue
+++ src/views/modules/product/spuadd.vue
@@ -381,10 +381,10 @@ export default {
       },
       spuBaseInfoRules: {
         spuName: [
-          { required: true, message: "请输入商品名字", trigger: "blur" }
+          { required: false, message: "请输入商品名字", trigger: "blur" }
         ],
         spuDescription: [
-          { required: true, message: "请编写一个简单描述", trigger: "blur" }
+          { required: false, message: "请编写一个简单描述", trigger: "blur" }
         ],
         catalogId: [
           { required: true, message: "请选择一个分类", trigger: "blur" }
@@ -393,10 +393,10 @@ export default {
           { required: true, message: "请选择一个品牌", trigger: "blur" }
         ],
         decript: [
-          { required: true, message: "请上传商品详情图集", trigger: "blur" }
+          { required: false, message: "请上传商品详情图集", trigger: "blur" }
         ],
         images: [
-          { required: true, message: "请上传商品图片集", trigger: "blur" }
+          { required: false, message: "请上传商品图片集", trigger: "blur" }
         ],
         weight: [
           {
