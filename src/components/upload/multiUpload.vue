92af685854bd17aa06080aa021873fb91b408072
xiaobailer <th15986581153@gmail.com>
2022-12-06 22:23:31 +0800

    前端spu商品添加功能【前台完成，后台功能待完善】

--- src/components/upload/multiUpload.vue
+++ src/components/upload/multiUpload.vue
@@ -82,7 +82,7 @@ export default {
             console.log("这是什么${filename}");
             _self.dataObj.policy = response.data.policy;
             _self.dataObj.signature = response.data.signature;
-            _self.dataObj.ossaccessKeyId = response.data.accessid;
+            _self.dataObj.ossaccessKeyId = response.data.accessId;
             _self.dataObj.key = response.data.dir + "/"+getUUID()+"_${filename}";
             _self.dataObj.dir = response.data.dir;
             _self.dataObj.host = response.data.host;
